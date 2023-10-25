8b84c62eb42ff324159727f8819616ede2a8cd29
Ruy-Araujo-Personal <ruy.araujo@yahoo.com.br>
2023-10-24 23:27:51 -0300

    feat: Add load to dw

--- airflow/dags/swapi/pipe/pipeline.py
+++ airflow/dags/swapi/pipe/pipeline.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-from swapi.dags import d01_extract, d02_transform
+from swapi.dags import d01_extract, d02_transform, d03_load
 
 default_args = {
     'owner': 'Data Engineer',
@@ -17,5 +17,6 @@ with DAG('swapi', default_args=default_args, schedule_interval=None) as dag:
 
     tsk1 = d01_extract.generate_dag(dag)
     tsk2 = d02_transform.generate_dag(dag)
+    tsk3 = d03_load.generate_dag(dag)
 
-    tsk1 >> tsk2
+    tsk1 >> tsk2 >> tsk3
